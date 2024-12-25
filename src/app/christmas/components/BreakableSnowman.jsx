c9c90119f758b5177845e53a467be668c25e4f14
JP Whitaker <jpw@hawaii.edu>
2024-12-24 23:38:12 -1000

    breakable snowman

--- src/app/christmas/components/BreakableSnowman.jsx
+++ src/app/christmas/components/BreakableSnowman.jsx
@@ -1,24 +1,37 @@
 import { Body } from './Body'
 import { Hat } from './Hat'
 import { Head } from './Head'
-import { RigidBody, CuboidCollider, BallCollider } from "@react-three/rapier"
+import { LowerBody } from './LowerBody'
+import { Base } from './Base'
+import { RigidBody, CuboidCollider, BallCollider, coll } from "@react-three/rapier"
 
 export function BreakableSnowman(props) {
   return (
     <group {...props}>
-      <RigidBody type="dynamic" colliders={false}>
+      <RigidBody type="dynamic" colliders={false} position={[0, 0.166, 0.1]}>
         <CuboidCollider args={[0.15, 0.12, 0.15]} position={[0, 0.51, 0]}/>
         <Hat />
       </RigidBody>
 
-      <RigidBody type="dynamic" colliders={false}>
-        <BallCollider args={[0.15, 0.12, 0.15]} position={[0, 0.25, 0]}/>
+      <RigidBody type="dynamic" colliders={false} position={[0, 0.13, 0.1]}>
+        <CuboidCollider args={[0.08, 0.01, 0.08]} position={[0, 0.4, 0.0]} name="headTop"/>
+        <BallCollider args={[0.14, 0.12]} position={[0, 0.25, 0]} name="headBall"/>
+        <CuboidCollider args={[0.08, 0.01, 0.08]} position={[0, 0.13, 0.0]} name="headBase"/>
         <Head />
+      </RigidBody>
+
+      <RigidBody type="dynamic" colliders={false} position={[0, 0.1, 0]}>
+        
+        <BallCollider args={[0.23, 0.12, 0.23]} position={[0.01, -0.1, 0.11]} name="bodyBall"/>
+        <CuboidCollider args={[0.1, 0.01, 0.1]} position={[0, 0.14, 0.11]} name="neck"/>
+        
+        <CuboidCollider args={[0.1, 0.01, 0.1]} position={[0, -0.34, 0.11]} name="bodyBase"/>
+        <LowerBody opacity={0.5} transparent={true}/>
       </RigidBody>
       
       <RigidBody type="fixed" colliders={false}>
-        <CuboidCollider args={[0.3, 0.35, 0.3]} position={[0, -0.2, 0]}/>
-        <Body />
+        <CuboidCollider args={[0.3, 0.2, 0.3]} position={[0, -0.45, 0]}/>
+        <Base />
       </RigidBody>
     </group>
   )
